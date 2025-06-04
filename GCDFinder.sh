8daeb7d89a1a6ec2a3d5571be1072959a7c72514
qraqras <q100rrraaa@gmail.com>
2025-06-04 12:54:09 +0000

    Add args check

--- GCDFinder.sh
+++ GCDFinder.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
-echo Hello, world!
+
+# エラーメッセージ
+ERR_MSG_ARG_CNT="引数が2つではありません"
+ERR_MSG_ARG_NUM="引数が自然数ではありません"
+
+################
+# エラー終了する
+# $1: エラーメッセージ
+# $2: エラーコード
+################
+EXIT_ERR ()  {
+    echo "$1" >&2
+    exit "$2"
+}
+
+# 引数の判定
+if [ $# -ne 2 ]; then
+    EXIT_ERR ${ERR_MSG_ARG_CNT} 1
+fi
+if [[ $1 =~ ^[0-9]+$ ]] && [[ $2 =~ ^[0-9]+$ ]] ; then
+    if [ $1 -ne 0 ] && [ $2 -ne 0 ]; then
+        :
+    else
+        EXIT_ERR ${ERR_MSG_ARG_NUM} 1
+    fi
+else
+    EXIT_ERR ${ERR_MSG_ARG_NUM} 1
+fi
 
--- Test_GCDFinder.sh
+++ Test_GCDFinder.sh
@@ -1,3 +1,65 @@
 #!/bin/bash
-echo Hello, world!
+
+tmp=/tmp/$$
+
+################
+# テストを実行する
+# $1  : テスト失敗時メッセージ
+# $2  : 想定結果(標準出力)
+# $3  : 想定結果(標準エラー出力)
+# $4-N: テスト引数
+################
+EXECUTE() {
+    # 第1引数から第3引数まで取得する
+    for i in {1..3}; do
+        args[$i]=$1
+        shift
+    done
+    # 想定結果を出力する
+    if [ "${args[2]}" = "_" ]; then
+        printf "" > ${tmp}-stdout-expected
+    else
+        printf "${args[2]}\n" > ${tmp}-stdout-expected
+    fi
+    if [ "${args[3]}" = "_" ]; then
+        printf "" > ${tmp}-stderr-expected
+    else
+        printf "${args[3]}\n" > ${tmp}-stderr-expected
+    fi
+    # 実行結果を出力する
+    ./GCDFinder.sh $@ 1> ${tmp}-stdout-actual 2> ${tmp}-stderr-actual
+    # 想定結果と実行結果を比較する
+    diff ${tmp}-stdout-expected ${tmp}-stdout-actual || EXIT_ERR ${args[1]}
+    diff ${tmp}-stderr-expected ${tmp}-stderr-actual || EXIT_ERR ${args[1]}
+}
+################
+# エラー終了する
+# $1: メッセージ
+################
+EXIT_ERR() {
+    # ファイルを削除する
+    rm -f ${tmp}-*
+    # エラー終了する
+    echo fail!: "$1" >&2
+    exit 1
+}
+
+
+EXECUTE "test_01" "_" "引数が2つではありません" "1"
+EXECUTE "test_02" "_" "引数が2つではありません" "A"
+EXECUTE "test_03" "_" "引数が2つではありません" "1" "2" "3"
+EXECUTE "test_04" "_" "引数が2つではありません" "A" "2" "3"
+EXECUTE "test_05" "_" "引数が自然数ではありません" "1" "A"
+EXECUTE "test_06" "_" "引数が自然数ではありません" "1" "1.5"
+EXECUTE "test_07" "_" "引数が自然数ではありません" "1" "0"
+# EXECUTE "test_0" "" "" ""
+
+# 合格
+echo "succeed!"
+# ファイルを削除する
+rm -f ${tmp}-*
+
+
+
+
 
