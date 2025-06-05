acafdbdf8c632da73309b861fc61fe0c94ce7d61
asai-r <asai-r@fuluhashi.co.jp>
2025-06-05 15:12:46 +0900

    Add Tests

--- GCDFinder.sh
+++ GCDFinder.sh
@@ -4,27 +4,30 @@
 ERR_MSG_ARG_CNT="引数が2つではありません"
 ERR_MSG_ARG_NUM="引数が自然数ではありません"
 
-################
-# エラー終了する
-# $1: エラーメッセージ
-# $2: エラーコード
-################
-EXIT_ERR ()  {
+################################
+## エラー終了する
+## $1: エラーメッセージ
+## $2: エラーコード
+################################
+EXIT_ERR()  {
     echo "$1" >&2
     exit "$2"
 }
 
-# 引数の判定
+# 引数の個数を判定
 if [ $# -ne 2 ]; then
     EXIT_ERR ${ERR_MSG_ARG_CNT} 1
 fi
-if [[ $1 =~ ^[0-9]+$ ]] && [[ $2 =~ ^[0-9]+$ ]] ; then
-    if [ $1 -ne 0 ] && [ $2 -ne 0 ]; then
-        :
+# 引数が自然数か判定
+for i in "$@"; do
+    if [[ $i =~ ^[0-9]+$ ]]; then
+        if [ $i -eq 0 ]; then
+            EXIT_ERR ${ERR_MSG_ARG_NUM} 1
+        fi
     else
         EXIT_ERR ${ERR_MSG_ARG_NUM} 1
     fi
-else
-    EXIT_ERR ${ERR_MSG_ARG_NUM} 1
-fi
+done
 
+
+exit 0
--- Test_GCDFinder.sh
+++ Test_GCDFinder.sh
@@ -1,65 +1,90 @@
 #!/bin/bash
 
+# テスト用ディレクトリ
 tmp=/tmp/$$
 
-################
-# テストを実行する
-# $1  : テスト失敗時メッセージ
-# $2  : 想定結果(標準出力)
-# $3  : 想定結果(標準エラー出力)
-# $4-N: テスト引数
-################
+################################
+## [[[[テストを実行する]]]]
+## $1  : テスト失敗時のメッセージ
+## $2  : 想定結果[標準出力]       (出力なしの場合は"_"、比較しない場合は"*"とする)
+## $3  : 想定結果[標準エラー出力] (出力なしの場合は"_"、比較しない場合は"*"とする)
+## $4-N: テスト実行時の引数
+################################
 EXECUTE() {
+    expected[1]=${tmp}-stdout-expected # 標準出力の想定結果
+    expected[2]=${tmp}-stderr-expected # 標準エラー出力の想定結果
+    actual[1]=${tmp}-stdout-actual     # 標準出力の想定結果
+    actual[2]=${tmp}-stderr-actual     # 標準エラー出力の想定結果
     # 第1引数から第3引数まで取得する
     for i in {1..3}; do
-        args[$i]=$1
+        args[$(expr $i - 1)]=$1
         shift
     done
-    # 想定結果を出力する
-    if [ "${args[2]}" = "_" ]; then
-        printf "" > ${tmp}-stdout-expected
-    else
-        printf "${args[2]}\n" > ${tmp}-stdout-expected
-    fi
-    if [ "${args[3]}" = "_" ]; then
-        printf "" > ${tmp}-stderr-expected
-    else
-        printf "${args[3]}\n" > ${tmp}-stderr-expected
-    fi
-    # 実行結果を出力する
-    ./GCDFinder.sh $@ 1> ${tmp}-stdout-actual 2> ${tmp}-stderr-actual
-    # 想定結果と実行結果を比較する
-    diff ${tmp}-stdout-expected ${tmp}-stdout-actual || EXIT_ERR ${args[1]}
-    diff ${tmp}-stderr-expected ${tmp}-stderr-actual || EXIT_ERR ${args[1]}
+    # 想定結果をファイル出力する
+    for i in {1..2}; do
+        # "_"のときは空ファイルにする
+        if [ "${args[$i]}" = "_" ]; then
+            printf "" > ${expected[$i]}
+        else
+            printf "${args[$i]}\n" > ${expected[$i]}
+        fi
+    done
+    # 実行結果をファイル出力する
+    ./GCDFinder.sh $@ 1> ${actual[1]} 2> ${actual[2]}
+    # 標準出力と標準エラー出力の内容を比較する
+    for i in {1..2}; do
+        #
+        if [ "${args[$i]}" != "*" ]; then
+            diff ${expected[$i]} ${actual[$i]} >/dev/null || EXIT_ERR ${args[0]}
+        fi
+    done
 }
-################
-# エラー終了する
-# $1: メッセージ
-################
-EXIT_ERR() {
+
+################################
+## [[[[終了する]]]]
+## $1: 終了コード
+################################
+EXIT() {
     # ファイルを削除する
     rm -f ${tmp}-*
-    # エラー終了する
-    echo fail!: "$1" >&2
-    exit 1
+    exit $1
+}
+
+################################
+## [[[[エラー終了する]]]]
+## $1: エラーメッセージ
+################################
+EXIT_ERR() {
+    echo "fail($1)" >&2
+    EXIT 1
 }
 
 
-EXECUTE "test_01" "_" "引数が2つではありません" "1"
-EXECUTE "test_02" "_" "引数が2つではありません" "A"
-EXECUTE "test_03" "_" "引数が2つではありません" "1" "2" "3"
-EXECUTE "test_04" "_" "引数が2つではありません" "A" "2" "3"
-EXECUTE "test_05" "_" "引数が自然数ではありません" "1" "A"
-EXECUTE "test_06" "_" "引数が自然数ではありません" "1" "1.5"
-EXECUTE "test_07" "_" "引数が自然数ではありません" "1" "0"
-# EXECUTE "test_0" "" "" ""
+# 引数のテスト(正常系)
+EXECUTE "test_00"    "*"    "_"                             "1"    "1"
+# 引数のテスト(異常系)
+EXECUTE "test_01"    "_"    "引数が2つではありません"       "1"
+EXECUTE "test_02"    "_"    "引数が2つではありません"       "A"
+EXECUTE "test_03"    "_"    "引数が2つではありません"       "1"    "2"    "3"
+EXECUTE "test_04"    "_"    "引数が2つではありません"       "0"    "2"    "3"
+EXECUTE "test_05"    "_"    "引数が自然数ではありません"    "1"    "A"
+EXECUTE "test_06"    "_"    "引数が自然数ではありません"    "1"    "0"
+EXECUTE "test_07"    "_"    "引数が自然数ではありません"    "1"    "-1"
+EXECUTE "test_08"    "_"    "引数が自然数ではありません"    "1"    "1.0"
+# 最大公約数のテスト(正常系)
+EXECUTE "test_09"    "1"    "_"    "1"    "2"
+EXECUTE "test_10"    "1"    "_"    "2"    "1"
+EXECUTE "test_11"    "1"    "_"    "3"    "4"
+EXECUTE "test_12"    "1"    "_"    "5"    "2"
+EXECUTE "test_13"    "2"    "_"    "2"    "4"
+EXECUTE "test_14"    "4"    "_"    "4"    "4"
+EXECUTE "test_15"    "11"   "_"    "121"  "209"
+EXECUTE "test_16"    "19"   "_"    "133"  "228"
+EXECUTE "test_17"    "28"   "_"    "112"  "252"
+EXECUTE "test_18"    "238"  "_"    "714"  "1666"
+EXECUTE "test_19"    "1"    "_"    "9967" "9973"
 
-# 合格
+
+# すべて合格
 echo "succeed!"
-# ファイルを削除する
-rm -f ${tmp}-*
-
-
-
-
-
+EXIT 0
